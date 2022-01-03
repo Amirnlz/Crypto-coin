0b882571524faaceaa6103f4984ac1822013b850
Amirnlz <Amiirnlz79@gmail.com>
2022-01-03 15:26:29 +0330

    add some methods for change format of numbers

--- lib/constant/extension/extension.dart
+++ lib/constant/extension/extension.dart
@@ -23,9 +23,19 @@ extension StringExtension on String {
 
 extension DoubleExtension on double {
   String get divideWithComma {
-    return toStringAsFixed(2).replaceAllMapped(
-        RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (match) => '${match[1]},');
+    return this >= 1
+        ? toStringAsFixed(2)
+            .replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                (match) => '${match[1]},')
+            .coinWithoutSign
+        : _lessThanNumberOne.coinWithoutSign;
   }
+
+  String get _lessThanNumberOne =>
+      this > 0 ? toStringAsFixed(4) : _lessThanZero;
+
+  String get _lessThanZero =>
+      this == 0 ? toStringAsFixed(2) : toStringAsFixed(5);
 
   String get getCoinStatusSign {
     if (toString().contains('-')) {
@@ -33,5 +43,23 @@ extension DoubleExtension on double {
     } else {
       return '+';
     }
+  }
+
+  String get thousandsFormatter {
+    if (this > 999 && this < 99999) {
+      return '${(this / 1000).toStringAsFixed(1)} K';
+    } else if (this > 99999 && this < 999999) {
+      return '${(this / 1000).toStringAsFixed(0)} K';
+    } else if (this > 999999 && this < 999999999) {
+      return '${(this / 1000000).toStringAsFixed(1)} M';
+    } else if (this > 999999999) {
+      return '${(this / 1000000000).toStringAsFixed(1)} B';
+    } else {
+      return toString();
+    }
+  }
+
+  double get removeDecimal {
+    return this >= 1 ? double.parse(toString().split('.').first) : this;
   }
 }
